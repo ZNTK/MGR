fd388cdf989d8800abf333884705f037aea34071
zntk <zntklol@gmail.com>
2018-08-27 23:28:39 +0200

    do wczesniejszego commita bo zapomnialem dodac

---
+++ MGR.WPF.DatabaseServices/DatabaseService.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGR.WPF.DatabaseServices
+{
+    public class DatabaseService
+    {
+        private MongoClient mongoClient;
+        private IMongoDatabase mongoDatabase;
+        public DatabaseService()
+        {
+            mongoClient = new MongoClient("mongodb://localhost:27017");
+            mongoDatabase = mongoClient.GetDatabase("MGR");
+        }
+
+        public IMongoQueryable<BsonDocument> Get(string collectionName)
+        {
+            return mongoDatabase.GetCollection<BsonDocument>(collectionName).AsQueryable();  
+        }
+
+        public List<IDictionary<String, Int32>> Get(string collectionName, string columnName)
+        {
+            var cos =  mongoDatabase.GetCollection<KeyValuePair<string,string>>(collectionName).Find(Builders<KeyValuePair<string, string>>.Filter.Empty).Project(Builders<KeyValuePair<string, string>>.Projection.Include(columnName).Exclude("_id")).ToList();
+            
+            List<IDictionary<String, Int32>> keyValuePairs = new List<IDictionary<String, Int32>>();
+            foreach (var item in cos)
+            {
+                keyValuePairs.Add(BsonSerializer.Deserialize<IDictionary<String, Int32>>(item));
+            }
+            var sasadas = keyValuePairs[0][columnName];
+
+            
+            return keyValuePairs;
+        }
+
+
+    }
+}
---
+++ MGR.WPF.MethodsServices/Filters/PearsonCorrelation.cs
@@ -0,0 +1,38 @@
+﻿using MGR.WPF.DatabaseServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGR.WPF.MethodsServices.Filters
+{
+    public class PearsonCorrelation
+    {
+        public PearsonCorrelation()
+        {
+
+        }
+        public void CompereTwoFeatures(List<Int32> featureX, List<Int32> featureY)
+        {
+            DatabaseService databaseService = new DatabaseService();
+
+            List<IDictionary<String, double>> featuresScore = new List<IDictionary<String, double>>();
+            var avgX = featureX.Average(x => x);
+            var avgY = featureY.Average(y => y);
+            //var ttt = featureX.Select(x => x.["Column1"]);
+            double sumUP = 0.00;
+            double sumDWX = 0.00;
+            double sumDWY = 0.00;
+            for (int i = 0; i < featureX.Count(); i++)
+            {
+                sumUP += (featureX[i] - avgX) * (featureY[i] - avgY);
+                sumDWX += Math.Pow((featureX[i] - avgX), 2);
+                sumDWY += Math.Pow((featureY[i] - avgY), 2);
+            }
+            var wynik = sumUP / (Math.Sqrt(sumDWX) * Math.Sqrt(sumDWY));
+        }
+            
+        
+    }
+}
