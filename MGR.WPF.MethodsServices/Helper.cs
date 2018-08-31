de566d3e29e94a5e6a495b2a8f9771c5586bc423
Michał Szyluk <mszyluk@x-code.pl>
2018-08-31 15:30:50 +0200

    pozatek rankingowania

---
+++ MGR.WPF.MethodsServices/Helper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGR.WPF.MethodsServices
+{
+    public class Helper
+    {
+        public void GetRankedTable(List<List<int>> table, int featuresCount)
+        {
+            List<List<int>> result = new List<List<int>>();
+
+            foreach (var item in table)
+            {
+
+            }
+        }
+
+        public void RankList(List<int> list)
+        {
+
+        }
+    }
+}
